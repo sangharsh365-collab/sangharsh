e54f7dd77ef4a9aea7c3f0a03ccce51607badd27
sangharsh365-collab <sangharsh365@gmail.com>
2025-08-22 23:19:36 +0530

    Create java.js

---
+++ java.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event){
+  event.preventDefault(); // Prevents page reload
+  alert("Thank you! Your message has been sent 🚀");
+});
