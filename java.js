1acfc75f4a2d35783ac48d3151de34a33e0f4a55
sangharsh365-collab <sangharsh365@gmail.com>
2025-08-30 20:31:29 +0530

    Delete java.js

--- java.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(event){
-  event.preventDefault(); // Prevents page reload
-  alert("Thank you! Your message has been sent 🚀");
-});
